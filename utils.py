3869963d99aa0ba8c5b62a20e07e03535cdcdeb7
viktorbeck98 <viktor.beck98@gmail.com>
2025-01-29 09:55:45 +0100

    small improvements

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import json
 from scholarly import scholarly
+import pylcs
 
 def load_json_bib(filename):
     """Requiring CSL Json export (from Zotero)."""
@@ -10,26 +11,25 @@ def get_json_bib_titles(json_bib: dict):
     """Get titles from CSL Json export (from Zotero) dictionary."""
     return [bib["title"].lower() for bib in json_bib]
 
-def lcs_length(str1, str2):
-    """Compute the length of the Longest Common Subsequence (LCS) between two strings."""
-    m, n = len(str1), len(str2)
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    for i in range(1, m + 1):
-        for j in range(1, n + 1):
-            if str1[i - 1] == str2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-    return dp[m][n]*2 / (len(str1)+len(str2))
+def jaccard_similarity(str1, str2):
+    set1, set2 = set(str1), set(str2)
+    intersection = len(set1.intersection(set2))
+    union = len(set1.union(set2))
+    return intersection / union
 
-def lcs_matrix_with_max(list1, list2, threshold=0.75):
+def lcs_length(str1, str2):
+    """Compute the harmonic mean of the length of the Longest Common Subsequence (LCS) between two strings."""
+    lcs = pylcs.lcs_sequence_length(str1, str2)
+    return lcs*2 / (len(str1)+len(str2)) # harmonic mean
+
+def similarity_matrix_with_max(list1, list2, threshold=0.75, similarity=lcs_length):
     """Create a matrix of LCS lengths and return elements corresponding to max LCS in each row."""
     matrix = []
     max_pairs = []
     for str1 in list1:
         row = []
         for str2 in list2:
-            row.append(lcs_length(str1, str2))
+            row.append(similarity(str1, str2))
         matrix.append(row)
         # Find the index of the maximum element in the row
         row_max = max(row)
@@ -41,19 +41,19 @@ def lcs_matrix_with_max(list1, list2, threshold=0.75):
             max_pairs.append((str1))
     return matrix, max_pairs
 
-def get_new_titles_since_last_search(initial_search_titles: list, new_search_titles:list, threshold=0.75):
+def get_new_titles_since_last_search(initial_search_titles: list, new_search_titles:list, threshold=0.75, similarity=lcs_length):
     """Compare two of titles (from different dates) and get the new titles. Compares titles based on LCS since they often have inconsistencies."""
     new_search_titles_lower = [t.lower() for t in new_search_titles]
-    matrix, max_pairs = lcs_matrix_with_max(new_search_titles_lower, initial_search_titles, threshold)
+    matrix, max_pairs = similarity_matrix_with_max(new_search_titles_lower, initial_search_titles, threshold, similarity=similarity)
     new_titles_since_last_search = [t for t in max_pairs if type(t) != tuple]
     return new_titles_since_last_search
 
-def get_new_titles_since_last_search_from_json(initial_search_export_path: str, new_search_export_path: str, threshold=0.7):
+def get_new_titles_since_last_search_from_json(initial_search_export_path: str, new_search_export_path: str, threshold=0.7, similarity=lcs_length):
     """Compare two json exports (from different dates) and get the new titles. Compares titles based on LCS since they often have inconsistencies. Requires CSL Json export (from Zotero)."""
     initial_search_titles = get_json_bib_titles(load_json_bib(initial_search_export_path))
     new_search_titles = get_json_bib_titles(load_json_bib(new_search_export_path))
     new_search_titles_lower = [t.lower() for t in new_search_titles]
-    matrix, max_pairs = lcs_matrix_with_max(new_search_titles_lower, initial_search_titles, threshold)
+    matrix, max_pairs = similarity_matrix_with_max(new_search_titles_lower, initial_search_titles, threshold, similarity=similarity)
     new_titles_since_last_search = [t for t in max_pairs if type(t) != tuple]
     return new_titles_since_last_search
 
